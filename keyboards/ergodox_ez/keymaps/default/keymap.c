0afd77014d1096f35711e03d9529fc59d383df59
Atsushi <nekiaiken@gmail.com>
2025-08-10 10:41:18 +0900

    Add files via upload

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -1,6 +1,9 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "i18n.h"
+#ifdef DYNAMIC_MACRO_ENABLE
+#include "process_dynamic_macro.h"
+#endif
 
 #ifndef ZSA_SAFE_RANGE
 #define ZSA_SAFE_RANGE SAFE_RANGE
@@ -23,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           JP_LBRC,                                        JP_RBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           JP_BSLS,
     KC_LEFT_CTRL,   KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_RIGHT_CTRL,
     LALT(LCTL(KC_DELETE)),KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           JP_GRV,                                         JP_QUOT,        KC_N,           KC_M,           JP_COMM,        JP_DOT,         JP_SLSH,        JP_EQL,
-    KC_TRANSPARENT, KC_LEFT_GUI,    KC_BSPC,        KC_LEFT_CTRL,   KC_LEFT_ALT,                                                                                                    KC_RIGHT_ALT,   KC_RIGHT_CTRL,  KC_BSPC,        JP_ZHTG,        LSFT(KC_F10),
+    KC_TRANSPARENT, KC_LEFT_GUI,    KC_BSPC,        KC_LEFT_SHIFT,  KC_LEFT_ALT,                                                                                                    KC_RIGHT_ALT,   KC_RIGHT_SHIFT,  KC_BSPC,        JP_ZHTG,       LSFT(KC_F10),
                                                                                                     KC_DOWN,        LCTL(JP_PLUS),  RCTL(JP_MINS),  KC_UP,
                                                                                                                     KC_HOME,        KC_INSERT,
                                                                                     KC_SPACE,       TD(DANCE_0),    KC_END,         KC_DELETE,      TD(DANCE_1),    LT(1, KC_ENTER)
@@ -32,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F11,                                         KC_F12,         KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_WH_LEFT,  KC_MS_UP,       KC_MS_WH_RIGHT, KC_MS_WH_UP,    KC_LPRN,                                        KC_RPRN,        KC_HOME,        KC_PAGE_UP,     KC_UP,          KC_PGDN,        KC_PSCR,        KC_F12,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_WH_DOWN,                                                                  KC_END,         KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LCTL(LSFT(KC_TAB)),                                LCTL(KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, DM_REC1,        DM_REC2,        DM_RSTP,        DM_PLY1,        DM_PLY2,        LCTL(LSFT(KC_TAB)),                                LCTL(KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_EQUAL,       KC_TRANSPARENT, LALT(KC_LEFT),  LALT(KC_RIGHT),                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_MS_BTN3,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
@@ -55,6 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // #ifdef DYNAMIC_MACRO_ENABLE
+  // if (!process_dynamic_macro(keycode, record)) return false;
+  // #endif
   switch (keycode) {
 
   }
